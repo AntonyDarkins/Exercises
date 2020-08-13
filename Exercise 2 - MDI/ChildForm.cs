51ab07606e90b5cab6fd2927bb2a82f8993d1c2a
antony darkins <antony.darkins@hotmail.co.uk>
2020-08-13 08:32:53 +0100

    initial draft, needs testing and comments

---
+++ Exercise 2 - MDI/ChildForm.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace Exercise_2___MDI
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+
+}
---
+++ Exercise 2 - MDI/ParentForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Exercise_2___MDI
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            int nextID = 0;
+            bool idOK = false;
+
+            while (!idOK)
+            {
+                idOK = true;
+                nextID++;
+                foreach (ChildForm child in this.MdiChildren.OfType<ChildForm>())
+                {
+                    if (child.FormID == nextID)
+                        idOK = false;
+                }
+            }
+
+            var consoleForm = new ChildForm(nextID);
+            consoleForm.Activated += ChildForm_Activated;
+            consoleForm.MdiParent = this;
+            consoleForm.Show();
+        }
+
+        private void cascadeWindowsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void tileWindowsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileVertical);
+        }
+
+        void ChildForm_Activated(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = ((Form)sender).Text;
+        }
+    }
+}
